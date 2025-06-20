964a48079c593dd355569662481dded12ab89c60
developer Calva <developercalva@gmail.com>
2025-06-20 12:13:24 -0600

    metodo para crear una nueva salida v2

--- routes/clientes.js
+++ routes/clientes.js
@@ -5,7 +5,7 @@ const clienteController = require("../controllers/clienteController");
 router.get("/", clienteController.getClientes);
 router.post("/", clienteController.crearCliente);
 router.put("/:id", clienteController.actualizarCliente);
-router.put("/nvaSalida", clienteController.actualizarTodosLosClientes);
+router.put("/", clienteController.actualizarTodosLosClientes);
 router.delete("/:id", clienteController.eliminarCliente);
 
 module.exports = router;
